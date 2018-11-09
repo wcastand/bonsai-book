54c05d5f45b8cc59edb4f29e97618723e7950dc0
William Castandet <wcastand@gmail.com>
2018-11-10 00:49:28 +0100

    remove .bonsai

--- .bonsai/pages/hello.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styled from 'react-emotion'
-
-const Hello = styled('h1')`
-  font-size: 2rem;
-  color: hotpink;
-`
-
-export default () => <Hello>Hello World.</Hello>
--- .bonsai/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import styled from 'react-emotion'
-
-const Home = () => <div>Home</div>
-
-export default Home
