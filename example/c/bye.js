cd86b9c6d3f8573b737c810bf7ae737406b08e43
William Castandet <wcastand@gmail.com>
2018-11-09 16:04:54 +0100

    add example

---
+++ example/c/bye.js
@@ -0,0 +1,6 @@
+import styled from 'react-emotion'
+
+export default styled('div')`
+  font-size: 4rem;
+  color: red;
+`
---
+++ example/stories/bye.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import Bye from '../c/bye'
+
+export default () => <Bye>Hello World.</Bye>
