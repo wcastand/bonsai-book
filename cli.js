708eebf58e667be74c0e986e8f5a9258a4a7774f
William Castandet <wcastand@gmail.com>
2018-11-13 18:03:38 +0100

    fix issue with path for published packages

--- cli.js
+++ cli.js
@@ -6,9 +6,11 @@ const path = require('path')
 const ncp = require('ncp')
 const chokidar = require('chokidar')
 
-const { project_dir, createSrcDir, copySrcFiles } = require('./cli_utils')
+const { project_dir, src_dir, createSrcDir, copySrcFiles } = require('./cli_utils')
+const bonsai = require(project_dir + '/server')
+const watcher = require(project_dir + '/watcher')
 
-const dev = process.env.NODE_ENV !== 'production'
+const superDev = process.env.NODE_ENV === 'devmode'
 
 const cli = meow(
   `
@@ -24,16 +26,13 @@ if (!fs.existsSync(project_dir)) {
   fs.mkdirSync(project_dir)
 }
 
-if (dev) {
+if (superDev) {
   const dev_watcher = chokidar.watch('./src/**/*')
   dev_watcher
     .on('add', copySrcFiles)
     .on('change', copySrcFiles)
     .on('addDir', createSrcDir)
     .on('ready', () => {
-      const bonsai = require(project_dir + '/server')
-      const watcher = require(project_dir + '/watcher')
-
       switch (cli.input[0]) {
         case 'start':
         default:
@@ -44,12 +43,16 @@ if (dev) {
       }
     })
 } else {
-  switch (cli.input[0]) {
-    case 'start':
-    default:
-      watcher(() => {
-        bonsai()
-      })
-      break
-  }
+  ncp(src_dir, project_dir, err => {
+    if (err) return console.error(err)
+
+    switch (cli.input[0]) {
+      case 'start':
+      default:
+        watcher(() => {
+          bonsai()
+        })
+        break
+    }
+  })
 }
--- cli_utils.js
+++ cli_utils.js
@@ -3,7 +3,6 @@
 const meow = require('meow')
 const fs = require('fs')
 const path = require('path')
-const ncp = require('ncp')
 const chokidar = require('chokidar')
 
 const project_dir = path.resolve(process.cwd(), './.bonsai')
@@ -21,11 +20,12 @@ const createSrcDir = dir_path => {
   const src_dir = path.resolve(process.cwd(), './src')
   const dest = path.resolve(process.cwd(), './.bonsai', path.relative(src_dir, dir_path))
   if (!fs.existsSync(dest))
-    fs.mkdir(dest, err => (err ? console.error(err) : console.info('direction created')))
+    fs.mkdir(dest, err => (err ? console.error(err) : console.info('directory created')))
 }
 
 module.exports = {
   project_dir,
+  src_dir,
   createSrcDir,
   copySrcFiles,
 }
